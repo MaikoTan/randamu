d9d4e436eb7d94a22cab17c204814eda0e98da17
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-26 10:51:55 +0800

    feat: add dateAfter / dateBefore params

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import requests
 from fastapi import FastAPI
 from starlette.responses import FileResponse 
@@ -5,7 +6,7 @@ from starlette.responses import FileResponse
 app = FastAPI()
 
 @app.get("/api")
-def main(tag=None, r18=0, num=1, excludeAI=True):
+def main(tag=None, r18=0, num=1, excludeAI=True, dateAfter: Optional[int]=None, dateBefore: Optional[int]=None):
     payload = {
         "r18": r18,
         "num": num,
@@ -13,6 +14,10 @@ def main(tag=None, r18=0, num=1, excludeAI=True):
     }
     if tag is not None:
         payload["tag"] = [tag]
+    if dateAfter is not None:
+        payload["dateAfter"] = dateAfter
+    if dateBefore is not None:
+        payload["dateBefore"] = dateBefore
     r = requests.post("https://api.lolicon.app/setu/v2", json=payload)
     j = r.json()
     i = j["data"][0]
