8298c3a4db1f2c4e4edbffe8f8ae0c9c4431c019
Maiko Tan <maiko.tan.coding@gmail.com>
2023-01-23 23:10:45 +0800

    refactor: move user-defined files to `user/` folder

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,5 @@ from app import routers
 app = FastAPI()
 
 app.include_router(routers.router)
+app.mount('/user', StaticFiles(directory='user'), name='user')
 app.mount('/', StaticFiles(directory='pages/dist', html=True), name='static')
---
+++ user/README.md
@@ -0,0 +1,3 @@
+# User Override Directory
+
+This is the place that you could place your file that would be append to the `index.html`, which allows you to override css style or add js files.
